cd3bb392ae9702a23a87cd1c25a87d12c8a57843
chaeyk <chaeyk@gmail.com>
2024-04-21 21:41:58 +0900

    로또 구매 구현 완료.

--- dhlottery.py
+++ dhlottery.py
@@ -1,20 +1,22 @@
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+
 from random import randint
 from os import getenv
+import traceback
 
 class DhLottery:
   driver: WebDriver
+  dryrun: bool
 
   def __init__(self, driver: WebDriver):
     self.driver = driver
+    self.dryrun = getenv('LTA_DRYRUN') == '1'
 
-  def login(self):
-    userid = getenv('DHL_USERID')
-    password = getenv('DHL_PASSWORD')
-
+  def login(self, userid: str, password: str):
     # 로그인 화면
     self.driver.get('https://dhlottery.co.kr/user.do?method=login&returnUrl=')
 
@@ -33,35 +35,99 @@ class DhLottery:
     if not name.text.endswith('님'):
       raise Exception(f'로그인 실패: {name.text}')
 
-  def buyLp72(self, count: int):
-    self.driver.get('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LP72')
+  def getBalance(self) -> str:
+    self.driver.get('https://dhlottery.co.kr')
 
-    iframe = self.driver.find_element(By.TAG_NAME, 'iframe')
-    self.driver.switch_to.frame(iframe)
+    money = self.driver.find_element(By.XPATH, '//li[@class="money"]/a/strong')
+    return money.text
 
-    for i in range(count):
-      jo = randint(1, 5)
-      jo_button = self.driver.find_element(By.XPATH, f'//span[@class="notranslate lotto720_box jogroup num{jo}"]')
-      jo_button.click()
+  # 로또 6/45
+  def buyLo40(self, count: int, dryrun: bool) -> str:
+    try:
+      self.driver.get('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40')
 
-      auto_button = self.driver.find_element(By.CLASS_NAME, 'lotto720_btn_auto_number')
-      auto_button.click()
+      iframe = self.driver.find_element(By.TAG_NAME, 'iframe')
+      self.driver.switch_to.frame(iframe)
 
-      confirm_button = self.driver.find_element(By.CLASS_NAME, 'lotto720_btn_confirm_number')
-      confirm_button.click()
+      # 자동 번호 선택
+      self.driver.execute_script('selectWayTab(1)')
 
-    buy1_button = self.driver.find_element(By.CLASS_NAME, 'lotto720_btn_pay')
-    buy1_button.click()
+      # 수량 선택
+      count_dropdown = Select(self.driver.find_element(By.ID, 'amoundApply'))
+      count_dropdown.select_by_value(str(count))
 
-    buy2_button = self.driver.find_element(By.XPATH, '//div[@id="lotto720_popup_confirm"]/div/div[@class="lotto720_popup_bottom_wrapper btn_area"]/a')
-    WebDriverWait(self.driver, 10).until(EC.visibility_of(buy2_button))
-    buy2_button.click()
+      # 수량 확인 버튼
+      select_num_button = self.driver.find_element(By.ID, 'btnSelectNum')
+      select_num_button.click()
 
-    # 구매 결과
+      print(f'dryrun {dryrun}')
+      if not dryrun:
+        # 구매 버튼 누름
+        buy_button = self.driver.find_element(By.NAME, 'btnBuy')
+        buy_button.click()
 
-    sale_span = self.driver.find_element(By.CLASS_NAME, 'saleCnt')
-    WebDriverWait(self.driver, 10).until(EC.visibility_of(sale_span))
-    if sale_span.text != str(count):
-      raise Exception('구매 실패건 있음')
+        # 구매 확인 누름
+        self.driver.execute_script('closepopupLayerConfirm(true)')
 
-    print(f'연금복권 구매완료: {count}매')
+        # 구매 결과 확인
+        report_row = self.driver.find_element(By.ID, 'reportRow')
+        WebDriverWait(self.driver, 10)\
+          .until(lambda driver: len(report_row.find_elements(By.XPATH, './li')) > 0)
+
+        report_count = len(report_row.find_elements(By.XPATH, './li'))
+        if count != report_count:
+          raise Exception(f'로또 구매 실패 {count - report_count}건 있음')
+
+      return f'로또 구매완료: {count}매'
+    except Exception as e:
+      print('로또 구매실패:', e)
+      traceback.print_exc()
+      return f'로또 구매실패: {e}'
+
+  # 연금복권 720+
+  def buyLp72(self, count: int, dryrun: bool) -> str:
+    try:
+      self.driver.get('https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LP72')
+
+      iframe = self.driver.find_element(By.TAG_NAME, 'iframe')
+      self.driver.switch_to.frame(iframe)
+
+      for i in range(count):
+        # 랜덤으로 조 선택
+        jo = randint(1, 5)
+        jo_button = self.driver.find_element(By.XPATH, f'//span[@class="notranslate lotto720_box jogroup num{jo}"]')
+        jo_button.click()
+
+        # 자동 번호 선택
+        auto_button = self.driver.find_element(By.CLASS_NAME, 'lotto720_btn_auto_number')
+        auto_button.click()
+
+        # 구매 등록
+        confirm_button = self.driver.find_element(By.CLASS_NAME, 'lotto720_btn_confirm_number')
+        confirm_button.click()
+
+      # 구매 버튼
+      buy1_button = self.driver.find_element(By.CLASS_NAME, 'lotto720_btn_pay')
+      buy1_button.click()
+
+      Alert(self.driver).accept()
+
+      if not dryrun:
+        # 구매 버튼이 하나 더 있음
+        buy2_button = self.driver.find_element(By.XPATH, '//div[@id="lotto720_popup_confirm"]/div/div[@class="lotto720_popup_bottom_wrapper btn_area"]/a')
+        WebDriverWait(self.driver, 10).until(EC.visibility_of(buy2_button))
+        buy2_button.click()
+
+        # 구매 결과
+
+        sale_span = self.driver.find_element(By.CLASS_NAME, 'saleCnt')
+        WebDriverWait(self.driver, 10).until(EC.visibility_of(sale_span))
+        sale_count = int(sale_span.text)
+        if sale_count != count:
+          raise Exception(f'연금복권 구매 실패 {count - sale_count}건 있음')
+
+      return f'연금복권 구매완료: {count}매'
+    except Exception as e:
+      print('연금복권 구매실패:', e)
+      traceback.print_exc()
+      return f'연금복권 구매실패: {e}'
--- main.py
+++ main.py
@@ -5,28 +5,58 @@ from os import getenv
 from time import sleep
 
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=True)
 
-headless = getenv('LTA_HEADLESS') == '1'
+def send_message(m: str):
+  print(m)
 
-chrome_options = Options()
-if headless:
-  chrome_options.add_argument('--headless')
-chrome_options.add_experimental_option('excludeSwitches', ['disable-popup-blocking'])
-chrome_options.set_capability('unhandledPromptBehavior', 'accept')
+def main():
+  userid = getenv('DHL_USERID')
+  password = getenv('DHL_PASSWORD')
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.implicitly_wait(10)
-driver.execute_cdp_cmd(
-  'Page.addScriptToEvaluateOnNewDocument',
-  {'source': "Object.defineProperty(navigator, 'platform', {get: () => 'Win64'})"}
-)
+  headless = getenv('LTA_HEADLESS') == '1'
+  lo40_count = int(getenv('LTA_LO40_COUNT') or '1')
+  lp72_count = int(getenv('LTA_LP72_COUNT') or '1')
+  dryrun = getenv('LTA_DRYRUN') == '1'
 
-dhlottery = DhLottery(driver)
-dhlottery.login()
-dhlottery.buyLp72(2)
+  chrome_options = Options()
+  if headless:
+    chrome_options.add_argument('--headless')
+  chrome_options.add_experimental_option('excludeSwitches', ['disable-popup-blocking']) # 팝업 차단
+  chrome_options.set_capability('unhandledPromptBehavior', 'accept') # alert 통과
 
-if not headless:
-  sleep(3600)
+  driver = webdriver.Chrome(options=chrome_options)
+  driver.implicitly_wait(10)
+  driver.execute_cdp_cmd( # navigator.platform 이 Win64를 리턴해야 모바일 페이지가 뜨지 않는다.
+    'Page.addScriptToEvaluateOnNewDocument',
+    {'source': "Object.defineProperty(navigator, 'platform', {get: () => 'Win64'})"}
+  )
 
-driver.quit()
+  message = '동행 복권 구매 결과입니다.\n'
+  def add_message(s: str):
+    nonlocal message
+    message = f'{message}\n{s}'
+
+  dhlottery = DhLottery(driver)
+  dhlottery.login(userid, password)
+  add_message(f'실행전 잔고: {dhlottery.getBalance()}')
+  if lo40_count > 0:
+    add_message(dhlottery.buyLo40(lo40_count, dryrun))
+  if lp72_count > 0:
+    add_message(dhlottery.buyLp72(lp72_count, dryrun))
+  
+  if headless:
+    add_message(f'실행후 잔고: {dhlottery.getBalance()}')
+
+  send_message(message)
+
+  if not headless:
+    sleep(3600)
+
+  driver.quit()
+
+try:
+  main()
+except Exception as e:
+  print(e)
+  raise
