02dc3fbb2295fd982cdafb96e4ae2ce6b0b05af1
chaeyk <chaeyk@gmail.com>
2024-04-30 08:47:50 +0900

    release.sh fix

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 mod=$(git status --porcelain)
 if [ -n "$mod" ]; then
@@ -9,15 +10,22 @@ if [ -n "$mod" ]; then
 fi
 
 current=$(git rev-parse --abbrev-ref HEAD)
+remote=origin
 branch="release/v1"
 
 if [ "$current" == "$branch" ]; then
   git push
 else
-  git branch "$branch" || true
-  git checkout "$branch"
-  git pull
-  git merge "$current"
-  git push
+  if git ls-remote --exit-code --heads "$remote" "$branch" > /dev/null 2>&1; then
+    # remote 브랜치가 있음
+    git fetch "$remote" "$branch"
+    git checkout "$branch"
+    git merge "$current"
+    git push "$remote" "$branch"
+  else
+    # remote 브랜치가 없음
+    git checkout -b "$branch"
+    git push --set-upstream "$remote" "$branch"
+  fi
   git checkout "$current"
 fi
